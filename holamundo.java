bf4ad7172d9e13f36bb2453899aa8de7e1bf0966
mangompae <mgomezpaez280705@gmail.com>
2024-11-04 17:21:48 +0100

    experimentoColores2

--- holamundo.java
+++ holamundo.java
@@ -2,7 +2,7 @@ class holamundo{
     public static void main(String[] args) {
         String rojo="\033[32m";
         String azul="\033[34m";
-        System.out.println("Hoal mundi");
-        System.out.println("Hoal mundi");
+        System.out.println(rojo+"Hoal mundi");
+        System.out.println(azul+"Hoal mundi");
 }
 }
